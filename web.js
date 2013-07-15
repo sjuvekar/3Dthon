920a8772ddfcb098949f560b1e02c7d16b64aaf1
Sudeep Juvekar <sjuvekar@gmail.com>
2013-07-15 19:38:13 -0400

    final logo

--- web.js
+++ web.js
@@ -4,7 +4,6 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  //response.send('Hello World 2!');
   var buf = fs.readFileSync('index.html');
   response.send(buf.toString());
 });

8696a0b00ea57a63daef5264653c895ee2e7cb8a
sjuvekar@gmail.com <ubuntu@ip-10-137-62-236.ec2.internal>
2013-07-18 19:06:50 +0000

    Dashboard

--- web.js
+++ web.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 // Variable devclaration
 var htmlfile = "index.html";
 var signupfile = "signup.html";
+var dashboardfile = "dashboard.html";
 
 var app = express.createServer(express.logger());
 
@@ -17,9 +18,16 @@ app.get('/', function(request, response) {
 
 // Signup page
 app.get('/signup', function(request, response) {
-  var html = fs.readFileSync(signupfile).toString();
-  response.send(html);
+  var sgnup = fs.readFileSync(signupfile).toString();
+  response.send(sgnup);
 });
+
+// User dashboard
+app.get('/dashboard', function(request, response) {
+  var dash = fs.readFileSync(dashboardfile).toString();
+  response.send(dash);
+});
+
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {

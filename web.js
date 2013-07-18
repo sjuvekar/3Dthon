e17db4715fcd6d064a85eff5db07f72197dca4db
sjuvekar@gmail.com <ubuntu@ip-10-137-62-236.ec2.internal>
2013-07-18 18:53:19 +0000

    Hypoerlink working

--- web.js
+++ web.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var fs = require('fs');
+
+// Variable devclaration
 var htmlfile = "index.html";
+var signupfile = "signup.html";
 
 var app = express.createServer(express.logger());
 
 app.use(express.static(__dirname + "/assets"));
 
+// Main page
 app.get('/', function(request, response) {
   var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
+});
+
+// Signup page
+app.get('/signup', function(request, response) {
+  var html = fs.readFileSync(signupfile).toString();
   response.send(html);
 });
 

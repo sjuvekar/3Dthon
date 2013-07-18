a9e19c5d82ae836316055b2ee37e6fa1e0554726
sjuvekar@gmail.com <ubuntu@domU-12-31-39-0B-DA-1A.compute-1.internal>
2013-07-18 15:58:12 +0000

    Standard web.js

--- web.js
+++ web.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buf = fs.readFileSync('index.html');
-  response.send(buf.toString());
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });

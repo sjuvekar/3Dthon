83c3811cbccba68fa70e48fe7fafaa7c6dad9771
Sudeep Juvekar <sjuvekar@tower-research.com>
2013-08-12 16:02:12 -0400

    IMP: Added postgres database for orders. Taken from bitstarter-ssjs-db
    Change-Id: Icd4605ab61efa5d51c94085a1adcc79096caf6d6

--- web.js
+++ web.js
@@ -1,14 +1,25 @@
-var express = require('express');
-var fs = require('fs');
+// Define routes for simple SSJS web app. 
+// Writes Coinbase orders to database.
+var async   = require('async')
+  , express = require('express')
+  , fs      = require('fs')
+  , http    = require('http')
+  , https   = require('https')
+  , db      = require('./models');
 
 // Variable devclaration
 var htmlfile = "index.html";
 var signupfile = "signup.html";
 var dashboardfile = "dashboard.html";
 
-var app = express.createServer(express.logger());
+var app = express();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || 8080);
 
-app.use(express.static(__dirname + "/assets"));
+
+//var app = express.createServer(express.logger());
+//app.use(express.static(__dirname + "/assets"));
 
 // Main page
 app.get('/', function(request, response) {
@@ -33,3 +44,96 @@ var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+// Code from bitstarter-ssjs-db. Adding orders to Database
+// Render example.com/orders
+app.get('/orders', function(request, response) {
+  global.db.Order.findAll().success(function(orders) {
+    var orders_json = [];
+    orders.forEach(function(order) {
+      orders_json.push({id: order.coinbase_id, amount: order.amount, time: order.time});
+    });
+    // Uses views/orders.ejs
+    response.render("orders", {orders: orders_json});
+  }).error(function(err) {
+    console.log(err);
+    response.send("error retrieving orders");
+  });
+});
+
+// Hit this URL while on example.com/orders to refresh
+app.get('/refresh_orders', function(request, response) {
+  https.get("https://coinbase.com/api/v1/orders?api_key=" + process.env.COINBASE_API_KEY, function(res) {
+    var body = '';
+    res.on('data', function(chunk) {body += chunk;});
+    res.on('end', function() {
+      try {
+        var orders_json = JSON.parse(body);
+        if (orders_json.error) {
+          response.send(orders_json.error);
+          return;
+        }
+        // add each order asynchronously
+        async.forEach(orders_json.orders, addOrder, function(err) {
+          if (err) {
+            console.log(err);
+            response.send("error adding orders");
+          } else {
+            // orders added successfully
+            response.redirect("/orders");
+          }
+        });
+      } catch (error) {
+        console.log(error);
+        response.send("error parsing json");
+      }
+    });
+
+    res.on('error', function(e) {
+      console.log(e);
+      response.send("error syncing orders");
+    });
+  });
+
+});
+
+// sync the database and start the server
+db.sequelize.sync().complete(function(err) {
+  if (err) {
+    throw err;
+  } else {
+    http.createServer(app).listen(app.get('port'), function() {
+      console.log("Listening on " + app.get('port'));
+    });
+  }
+});
+
+// add order to the database if it doesn't already exist
+var addOrder = function(order_obj, callback) {
+  var order = order_obj.order; // order json from coinbase
+  if (order.status != "completed") {
+    // only add completed orders
+    callback();
+  } else {
+    var Order = global.db.Order;
+    // find if order has already been added to our database
+    Order.find({where: {coinbase_id: order.id}}).success(function(order_instance) {
+      if (order_instance) {
+        // order already exists, do nothing
+        callback();
+      } else {
+        // build instance and save
+          var new_order_instance = Order.build({
+          coinbase_id: order.id,
+          amount: order.total_btc.cents / 100000000, // convert satoshis to BTC
+          time: order.created_at
+        });
+          new_order_instance.save().success(function() {
+          callback();
+        }).error(function(err) {
+          callback(err);
+        });
+      }
+    });
+  }
+};

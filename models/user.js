a134bbf97cd26fee14f3a3e9764690f5a638316c
Sudeep Juvekar <sjuvekar@tower-research.com>
2013-08-26 17:46:24 -0400

    Modified user.js to use mongoDB
    Change-Id: I57d9c6e373702ea19a0203364dfc31488da526f9

--- models/user.js
+++ models/user.js
@@ -1,15 +1,20 @@
-/* Object/Relational mapping for instances of the User class.
-     - classes correspond to tables
-     - instances correspond to rows
-    - fields correspond to columns
-In other words, this code defines how a row in the postgres order table
-maps to the JS Order object.
-*/
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define("User", {
-    user_id: {type: DataTypes.STRING, unique: true, allowNull: false},
-    name: {type: DataTypes.STRING},
-    auth_as: {type: DataType.STRING},
-    time: {type: DataTypes.STRING, allowNull: false}
-  });
-};
+var mongoose = require("mongoose");
+
+var UserSchema = mongoose.schema({
+    email: {
+	type: String,
+	required: true,
+	index: { unique: true }
+    },
+    password: {
+	type: String,
+	required: true
+    },
+    name: {
+	type: String,
+	required: true
+    }
+});
+
+module.exports = mongoose.model(User&, UserSchema);
+    

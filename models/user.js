e2e937f8256a43f9242500c75a24e7778d00d608
Sudeep Juvekar <sjuvekar@tower-research.com>
2013-08-26 17:59:44 -0400

    Password comparison
    Change-Id: I54dd6336a7790a4eb59551f42efbf11347cfdef9

--- models/user.js
+++ models/user.js
@@ -16,5 +16,32 @@ var UserSchema = mongoose.schema({
     }
 });
 
+UserSchema.pre(‘save’, { 
+    var user = this;
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+ 
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+	if (err) return next(err);
+ 
+	// hash the password along with our new salt
+	bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+ 
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+	});
+    });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
 module.exports = mongoose.model(User&, UserSchema);
     

68037f1797d48b5d9e3a04abdb71b68e594825c7
Sudeep Juvekar <sjuvekar@gmail.com>
2013-09-27 15:42:58 +0000

    Removing sequalize

--- web.js
+++ web.js
@@ -28,6 +28,9 @@ app.use(express.session({ secret: 'keyboard cat' }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
+http.createServer(app).listen(app.get('port'), function() {
+      console.log("Listening on " + app.get('port'));
+});
 
 // Initialize mongoose DB
 mongooseDB.mongooseInit();
@@ -48,19 +51,7 @@ passport.deserializeUser(function(id, done) {
 
 // Main page
 app.get('/', function(request, response) {
-  global.db.Order.findAll().success(function(orders) {
-    var total_backers = orders.length;
-    var backer_percent = total_backers;
-    if (backer_percent > 100) backer_percent = 100;
-    var total_bitcoins = 0;
-    orders.forEach(function(order) {
-	total_bitcoins += order.amount;
-    });
-    response.render("index", {backers: total_backers, percent: backer_percent, bitcoins: total_bitcoins.toFixed(5)});  
-  }).error(function(err) {
-      console.log(error);
-      response.render("index", {backers: 1, bitcoins: 0.00001});  
-  });
+    response.render("index");
 });
 
 // Signup and Signout
@@ -103,96 +94,3 @@ app.post("/local_signin", localAuth.local_signin());
 app.post("/local_signup", function(request, response) { localAuth.local_signup(request, response); });
 app.post("/post_contest", function(request, response) { route.postContest(request, response); });
 	 
-
-// Code from bitstarter-ssjs-db. Adding orders to Database
-// Render example.com/orders
-app.get('/orders', function(request, response) {
-  global.db.Order.findAll().success(function(orders) {
-    var orders_json = [];
-    orders.forEach(function(order) {
-      orders_json.push({id: order.coinbase_id, amount: order.amount, time: order.time});
-    });
-    // Uses views/orders.ejs
-    response.render("orders", {orders: orders_json});
-  }).error(function(err) {
-    console.log(err);
-    response.send("error retrieving orders");
-  });
-});
-
-// Hit this URL while on example.com/orders to refresh
-app.get('/refresh_orders', function(request, response) {
-  https.get("https://coinbase.com/api/v1/orders?api_key=" + process.env.COINBASE_API_KEY, function(res) {
-    var body = '';
-    res.on('data', function(chunk) {body += chunk;});
-    res.on('end', function() {
-      try {
-        var orders_json = JSON.parse(body);
-        if (orders_json.error) {
-          response.send(orders_json.error);
-          return;
-        }
-        // add each order asynchronously
-        async.forEach(orders_json.orders, addOrder, function(err) {
-          if (err) {
-            console.log(err);
-            response.send("error adding orders");
-          } else {
-            // orders added successfully
-            response.redirect("/orders");
-          }
-        });
-      } catch (error) {
-        console.log(error);
-        response.send("error parsing json");
-      }
-    });
-
-    res.on('error', function(e) {
-      console.log(e);
-      response.send("error syncing orders");
-    });
-  });
-
-});
-
-// sync the database and start the server
-db.sequelize.sync().complete(function(err) {
-  if (err) {
-    throw err;
-  } else {
-    http.createServer(app).listen(app.get('port'), function() {
-      console.log("Listening on " + app.get('port'));
-    });
-  }
-});
-
-// add order to the database if it doesn't already exist
-var addOrder = function(order_obj, callback) {
-  var order = order_obj.order; // order json from coinbase
-  if (order.status != "completed") {
-    // only add completed orders
-    callback();
-  } else {
-    var Order = global.db.Order;
-    // find if order has already been added to our database
-    Order.find({where: {coinbase_id: order.id}}).success(function(order_instance) {
-      if (order_instance) {
-        // order already exists, do nothing
-        callback();
-      } else {
-        // build instance and save
-          var new_order_instance = Order.build({
-          coinbase_id: order.id,
-          amount: order.total_btc.cents / 100000000, // convert satoshis to BTC
-          time: order.created_at
-        });
-          new_order_instance.save().success(function() {
-          callback();
-        }).error(function(err) {
-          callback(err);
-        });
-      }
-    });
-  }
-};

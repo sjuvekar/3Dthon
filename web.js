6120595806e131be83634ff4cb123d5978718259
Sudeep Juvekar <sjuvekar@gmail.com>
2015-05-23 13:13:15 -0400

    CORS

--- web.js
+++ web.js
@@ -27,6 +27,25 @@ app.use(express.session({ secret: 'keyboard cat' }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://s3-us-west-1.amazonaws.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 http.createServer(app).listen(app.get('port'), function() {
       console.log("Listening on " + app.get('port'));
 });

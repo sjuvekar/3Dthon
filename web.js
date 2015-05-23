fa13dfcc0241afbc81c9b5900d18fb542b65c7f1
Sudeep Juvekar <sjuvekar@gmail.com>
2015-05-23 13:19:42 -0400

    CORS

--- web.js
+++ web.js
@@ -35,13 +35,6 @@ app.use(function (req, res, next) {
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
     // Pass to next layer of middleware
     next();
 });
